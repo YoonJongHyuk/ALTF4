ce04ffd027f346c19edc795b31868fcfe5a72fb5
YoonJongHyuk <mana951@naver.com>
2024-07-01 18:28:11 +0900

    네번째 스테이지 제작 외
    네번째 스테이지 제작 중
    시네머신 카메라 테스트 중

--- Assets/Scripts/jonghyuk/TestPlayerMove.cs
+++ Assets/Scripts/jonghyuk/TestPlayerMove.cs
@@ -4,20 +4,58 @@ using UnityEngine;
 
 public class TestPlayerMove : MonoBehaviour
 {
-    public float testPlayerMove = 0.0f;
-    // Start is called before the first frame update
-    void Start()
+    public float maximumSpeed = 3;
+    public float rotationSpeed = 720;
+    public Transform cameraTransform;
+
+    private void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-        Vector3 dir = new Vector3(h, 0, v);
-        dir.Normalize();
-        transform.position += dir * testPlayerMove * Time.deltaTime;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+
+        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
+        float speed = inputMagnitude * maximumSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            inputMagnitude *= 0.5f;
+        }
+
+        // 카메라의 y회전만으로 새회전을 생성
+        // 카메라의 y회전을 이동 방향에 적용
+        movementDirection = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * movementDirection;
+        movementDirection.Normalize();
+
+        Vector3 velocity = movementDirection * speed;
+
+        // 단순한 이동 로직
+        transform.Translate(velocity * Time.deltaTime, Space.World);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        // 어플리케이션이 포커스를 받으면 커서를 숨긴다.
+        if (focus)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
