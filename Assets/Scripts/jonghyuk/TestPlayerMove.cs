5b0f59610048e959d475f29cf83c5a9d16aa0845
YoonJongHyuk <mana951@naver.com>
2024-07-02 18:05:49 +0900

    시네머신 카메라 외
    시네머신 카메라 적용
    (Y축 180 이상일 때 끊김 현상 고쳐야 함.)
    Scene 이동 구현
    움직임 개선(Y축 회전 개선해야 함.)

--- Assets/Scripts/jonghyuk/TestPlayerMove.cs
+++ Assets/Scripts/jonghyuk/TestPlayerMove.cs
@@ -4,51 +4,82 @@ using UnityEngine;
 
 public class TestPlayerMove : MonoBehaviour
 {
-    public float maximumSpeed = 3;
-    public float rotationSpeed = 720;
+    public float maximumSpeed = 3f;
+    public float jumpForce = 10f;
+    public float rotationSpeed = 720f;
+    private bool isJumping;
+
     public Transform cameraTransform;
+    private Rigidbody rb;
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        isJumping = false;
     }
 
     private void Update()
+    {
+        Jump();
+        RotatePlayer();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-
         float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
         float speed = inputMagnitude * maximumSpeed;
 
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-        {
-            inputMagnitude *= 0.5f;
-        }
-
-        // 카메라의 y회전만으로 새회전을 생성
-        // 카메라의 y회전을 이동 방향에 적용
         movementDirection = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * movementDirection;
         movementDirection.Normalize();
 
         Vector3 velocity = movementDirection * speed;
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
 
-        // 단순한 이동 로직
-        transform.Translate(velocity * Time.deltaTime, Space.World);
+    private void RotatePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
         if (movementDirection != Vector3.zero)
         {
+            movementDirection = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * movementDirection;
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
     }
 
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            isJumping = true;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+        }
+    }
+
     private void OnApplicationFocus(bool focus)
     {
-        // 어플리케이션이 포커스를 받으면 커서를 숨긴다.
         if (focus)
         {
             Cursor.lockState = CursorLockMode.Locked;
